d2c885315a14b06c9a994823c22d56d2fa6d890c
nullTheCoder <stepan.gaidukevich@gmail.com>
2023-02-13 05:53:45 +0200

    Fixed some problems (I think?)

--- src/test/java/com/voicesofwynn/tests/InitTest.java
+++ src/test/java/com/voicesofwynn/tests/InitTest.java
@@ -6,13 +6,6 @@ import org.junit.jupiter.api.Test;
 public class InitTest {
     @Test
     void init() {
-        // fail test
-        try {
-            VOWCore.init(null, null);
-            assert false;
-        } catch (IllegalArgumentException ignore) {
-
-        }
 
     }
 }
