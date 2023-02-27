7c47fd0a9df81e86d6247dd21c4583642fbaf19a
nullBlade <stepan.gaidukevich@gmail.com>
2023-02-27 12:18:08 +0200

    small fix

--- src/main/java/com/voicesofwynn/core/loadmanager/types/DialogType.java
+++ src/main/java/com/voicesofwynn/core/loadmanager/types/DialogType.java
@@ -1,6 +1,7 @@
 package com.voicesofwynn.core.loadmanager.types;
 
 import com.voicesofwynn.core.loadmanager.RegisterType;
+import com.voicesofwynn.core.loadmanager.WriteInstanceValues;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -13,7 +14,8 @@ public class DialogType implements RegisterType {
     }
 
     @Override
-    public void write(FileOutputStream writer, Object section, Map<String, Object> vars) {
+    public void write(FileOutputStream writer, Object section, WriteInstanceValues value) {
+
     }
 
     @Override
