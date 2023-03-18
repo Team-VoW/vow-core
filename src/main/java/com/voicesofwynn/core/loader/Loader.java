9179618ed8064e236d91915ff86d5a8dcbc797c1
nullBlade <stepan.gaidukevich@gmail.com>
2023-03-18 11:13:56 +0200

    starting loader work

---
+++ src/main/java/com/voicesofwynn/core/loader/Loader.java
@@ -0,0 +1,18 @@
+package com.voicesofwynn.core.loader;
+
+import com.voicesofwynn.core.VOWCore;
+
+import java.io.File;
+
+public class Loader {
+
+    private File base;
+
+    public Loader () {
+        base = new File(VOWCore.getRootFolder(), "files");
+
+
+
+    }
+
+}
