bc77fc4a97b2c4098f1394ce722eb17ba869cb9f
nullBlade <stepan.gaidukevich@gmail.com>
2023-04-23 10:51:55 +0300

    revert

--- src/test/java/com/voicesofwynn/tests/loadmanager/typetests/DialogTypeTest.java
+++ src/test/java/com/voicesofwynn/tests/loadmanager/typetests/DialogTypeTest.java
@@ -21,8 +21,8 @@ public class DialogTypeTest {
 
         File testOut = new File(TestSettings.TEST_DIR, "loadManager/type/dialogTests/test_a");
 
-        loadManager.build(new File("testing/files/loadmanager/individual/dialog_tests/test_a.yml"),
-                testOut, new File("testing/files/loadmanager/individual/dialog_tests/"));
+        loadManager.build(new File("files/loadmanager/individual/dialog_tests/test_a.yml"),
+                testOut, new File("files/loadmanager/individual/dialog_tests/"));
 
         LoadManager.getInstance().load(testOut);
 
